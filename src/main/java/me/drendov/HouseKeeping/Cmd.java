96c5d46d53940e9e47ca7b42f89ab6a1e7f9b88d
Alexey Filich <alexey.filich@mail.ru>
2021-08-09 18:13:04 +0300

    HK-3 Develop players absence tracking

--- src/main/java/me/drendov/HouseKeeping/Cmd.java
+++ src/main/java/me/drendov/HouseKeeping/Cmd.java
@@ -2,47 +2,47 @@ package me.drendov.HouseKeeping;
 
 import java.util.logging.Logger;
 
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
-public class Cmd
-        implements CommandExecutor {
+public class Cmd implements CommandExecutor {
     private static HouseKeeping plugin = HouseKeeping.getInstance();
     private static Logger logger = plugin.getLogger();
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-
         Player player = null;
         if (sender instanceof Player) {
-            player = (Player) sender;
+            player = (Player)sender;
         }
 
         if (cmd.getName().equalsIgnoreCase("housekeeping")) {
-            if (sender.hasPermission("housekeeping.reload") || sender.isOp()) {
-                try {
-                    String playerName = "";
-                    if (args.length > 0) {
-                        if (!args[0].contains(":")) {
-                            playerName = args[0];
-                        }
-                    } else {
-                        plugin.showInfo(sender);
-                        return true;
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
+            if (args.length == 0) {
+                plugin.showInfo(sender);
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("absent")) {
+                if (!sender.hasPermission("housekeeping.absent") && !sender.isOp()) {
+                    HouseKeeping.sendMessage(player, TextMode.Err, Messages.NoPermissionForCommand);
+                    return true;
                 }
+                HouseKeeping.checkAbsence();
+                return true;
+            } else if (args[0].equalsIgnoreCase("reload")) {
+                if (!sender.hasPermission("housekeeping.reload") && !sender.isOp()) {
+                    HouseKeeping.sendMessage(player, TextMode.Err, Messages.NoPermissionForCommand);
+                    return true;
+                }
+                plugin.reloadConfig();
+                return true;
             } else {
-                HouseKeeping.sendMessage(player, TextMode.Err, Messages.NoPermissionForCommand);
+                // Command not found
+                plugin.showInfo(sender);
                 return true;
             }
         }
         return false;
     }
-
 }
-
