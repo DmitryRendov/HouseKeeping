d01d46e12d9a0e488e5db8a960e8bef7a518626b
Dmitry Rendov <dmitry_rendov@epam.com>
2021-08-09 11:19:02 +0300

    10 - Disable some commands based on coords

--- src/main/java/me/drendov/HouseKeeping/Listeners.java
+++ src/main/java/me/drendov/HouseKeeping/Listeners.java
@@ -1,10 +1,14 @@
 package me.drendov.HouseKeeping;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.logging.Logger;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryOpenEvent;
-
-import java.util.logging.Logger;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class Listeners
         implements Listener {
@@ -12,11 +16,49 @@ public class Listeners
     private static Logger logger = plugin.getLogger();
 
     @EventHandler
-    public void inventoryOpenListener(InventoryOpenEvent event) {
-        String player = event.getPlayer().getName();
+    public void onPlayerCommandPreProcess(final PlayerCommandPreprocessEvent event) {
 
-        HouseKeeping.sendMessage(null, TextMode.Instr,
-                "User " + event.getPlayer().getName());
+        Player player = null;
+        if (event.getPlayer() instanceof Player) {
+            player = event.getPlayer();
+        }
+
+        if (player.isOp() || player.hasPermission("housekeeping.bypass"))
+            return;
+
+        GameMode mode = player.getGameMode();
+        if (mode != GameMode.CREATIVE && mode != GameMode.SPECTATOR) {
+            HashMap getSafeZoneArea = HouseKeeping.getInstance().config.getSafeZoneArea(player);
+            Location loc1 = (Location) getSafeZoneArea.get("loc1");
+            Location loc2 = (Location) getSafeZoneArea.get("loc2");
+            if (this.isInRect(player, loc1, loc2)) {
+                for (final String command : HouseKeeping.getInstance().config.getSafezoneBlockedCommands()) {
+                    if (event.getMessage().toLowerCase().equals("/" + command) || event.getMessage().toLowerCase().startsWith("/" + command + " ")) {
+                        event.setCancelled(true);
+                        HouseKeeping.sendMessage(player, TextMode.Err, Messages.DenyCommandMsg);
+                    }
+                }
+            }
+        }
+
+    }
+
+    public boolean isInRect(Player player, Location loc1, Location loc2) {
+        double[] dim = new double[2];
+
+        dim[0] = loc1.getX();
+        dim[1] = loc2.getX();
+        Arrays.sort(dim);
+        if (player.getLocation().getX() > dim[1] || player.getLocation().getX() < dim[0])
+            return false;
+
+        dim[0] = loc1.getZ();
+        dim[1] = loc2.getZ();
+        Arrays.sort(dim);
+        if (player.getLocation().getZ() > dim[1] || player.getLocation().getZ() < dim[0])
+            return false;
+
+        return true;
     }
 }
 
--- src/main/java/me/drendov/HouseKeeping/Messages.java
+++ src/main/java/me/drendov/HouseKeeping/Messages.java
@@ -4,5 +4,6 @@ public enum Messages
 {
     Reloaded,
     NoPermissionForCommand,
-    ReloadMsg
+    ReloadMsg,
+    DenyCommandMsg
 }
